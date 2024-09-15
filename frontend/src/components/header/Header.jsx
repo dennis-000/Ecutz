50fd6d021f8b4f43ee1b2ed21e60fac7488bb8b4
Dennis Asiedu <odennisasiedu@gmail.com>
2024-09-15 16:08:39 +0000

    Will continue -- Nav Process

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import {useEffect, useRef} from 'react'
+import logo from "../../assets/images/ecutz.png";
+
+import { NavLink, Link } from "react-router-dom";
+
+// ========== CODDED ========
+// Nav Path Links
+const navLinks = [
+  {
+    path: '/home',
+    display: 'Home'
+  },
+  {
+    path: '/barbers',
+    display: 'Find a Barber'
+  },
+  {
+    path: '/services',
+    display: 'Services'
+  },
+  {
+    path: '/contact',
+    display: 'Contact'
+  },
+  
+]
+
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify justify-between">
+      {/* ========= logo =========== */}
+          <div>
+            <img src={logo} alt="Ecutz Logo" style={{ width: '80px', height: '80px' }} />
+          </div>
 
-export default Header
+
+          {/* ========= MENU  ========= */}
+          <div className="navigation">
+            <ul className='menu flex items-center gap-[2.7rem]'>
+              {/* Addin all the necessary attributes here to modify the  the nav bar */}
+              {
+                navLinks.map((link, index) =><li key={index}>
+                  <NavLink to={link.path} className={navClass => navClass.isActive
+                    ? 'text-primaryColor text-[16px] leading-7 font-[600]'
+                    : 'text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor'
+                  }
+                  >
+                    {link.display}
+                  </NavLink>
+                </li>
+                )}
+            </ul>
+          </div>
+         
+          {/* =========== nav right ============ */}
+          <div>
+            
+          </div>
+
+        </div>
+      </div>
+  </header>
+  );
+};
+
+export default Header;
