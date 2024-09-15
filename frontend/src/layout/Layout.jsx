49f9492c792c68a10ef2cee0a56d0d03919b1b9c
Dennis Asiedu <odennisasiedu@gmail.com>
2024-09-15 01:42:45 +0000

    Start...

---
+++ frontend/src/layout/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Header from '../components/header/Header';
+import Footer from '../components/footer/Footer';
+import Routers from '../routes/Routers';
+const Layout = () => {
+  return <>
+   <Header/>
+       <main>
+           <Routers/>
+      </main>
+      <Footer>
+          
+      </Footer>
+    </>
+    
+    
+};
+
+export default Layout
