5bb171b5769975838b46ef31bb5b7bd409bbc076
Dennis Asiedu <odennisasiedu@gmail.com>
2024-09-16 02:44:36 +0000

    Done with the header moving to the contents

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,7 +1,12 @@
-import {useEffect, useRef} from 'react'
+import { useEffect, useRef } from 'react'
+// ============= Logo importation
 import logo from "../../assets/images/ecutz.png";
+// ===============Image importation --- Profile
+import userImg from '../../assets/images/avatar-icon.png';
 
 import { NavLink, Link } from "react-router-dom";
+import { BiMenu } from 'react-icons/bi';
+
 
 // ========== CODDED ========
 // Nav Path Links
@@ -27,18 +32,41 @@ const navLinks = [
 
 
 const Header = () => {
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        headerRef.current.classList.add('sticky__header')
+      } else {
+        headerRef.current.classList.remove('sticky__header')
+      }
+    })
+  }
+  
+  useEffect(() => {
+    handleStickyHeader();
+
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  });
+
+  const toggleMenu = ()=> menuRef.current.classList.toggle('show__menu')
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify justify-between">
       {/* ========= logo =========== */}
           <div>
-            <img src={logo} alt="Ecutz Logo" style={{ width: '80px', height: '80px' }} />
+            <img src={logo} alt="Ecutz Logo" style={{ width: '70px', height: '70px' }} />
           </div>
 
 
           {/* ========= MENU  ========= */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className='menu flex items-center gap-[2.7rem]'>
               {/* Addin all the necessary attributes here to modify the  the nav bar */}
               {
@@ -56,8 +84,29 @@ const Header = () => {
           </div>
          
           {/* =========== nav right ============ */}
-          <div>
+          <div className='flex items-center gap-4'>
+            <div className='hidden'>
+              <Link to='/'>
+                {/* User Profile Picture --- with it's styling */}
+                <figure className='w-[35px] rounded-full cursor-pointer'>
+                  <img src={userImg} className="w-full rounded-full" alt="The User Image" />
+                </figure>
+              </Link>
+            </div>
+
             
+{/* =============== Login Button ================ */}
+            <Link to='/login'>
+              <button className='bg-primaryColor py-6 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>
+                Login
+              </button>
+            </Link>
+        {/* ========== Nav Image =======  */}
+            <span className='md:hidden' onClick={toggleMenu}>
+              <BiMenu className='w-6 h-6 cursor-pointer'/>
+            </span>
+
+
           </div>
 
         </div>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  return <>
+    {/* ====== Hero Section ========== */}
+    <section className='hero__section pt-[60px] 2xl:h-[800px]'>
+      <div className="container">
+        <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
+          {/* =========== CONTENT ========== */}
+          <div>
+            <div className='lg:w-[570px]'>
+              <h1></h1>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+  </>
 }
 
-export default Home
+export default Home;
