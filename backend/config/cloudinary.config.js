ea227a14f3b30e3a09b51b556a1ba0b3a71be6c5
Henry <henryasante336@gmail.com>
2024-11-20 06:36:21 +0000

    Finished login and audit log creation logic. Also started cloudinary upload

---
+++ backend/config/cloudinary.config.js
@@ -0,0 +1,17 @@
+import { v2 as cloudinary } from 'cloudinary'
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
+// // Check if CLOUDINARY_URL exists
+// if (!process.env.CLOUDINARY_CLOUD_NAME) {
+//   throw new Error('CLOUDINARY_CLUDNAME is not set in .env file');
+// }
+// Configure Cloudinary using the URL from .env
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+export default cloudinary
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,10 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./config/db.js"
-import User from "./models/user.model.js"
-import Service from "./models/service.model.js"
-import Appointment from "./models/appointment.model.js"
-import Review from "./models/review.model.js"
-import AuditLog from "./models/audit.model.js"
 import multer from "multer"
 import bcrypt from "bcrypt"
-import ProviderService from "./models/providerService.model.js"
-import fs from 'fs';
-import path from 'path';
 import updateExpiredAppointments from './controllers/cron.controller.js'; // Your cron job
 import cron from 'node-cron'
-import { createNewUser, deleteUser, getAllCustomers, getAllProviders, getAllUsers, getSingleUserById, updateUser } from "./controllers/user.controller.js"
-import { createNewProviderService, deleteProviderService, getAllProviderService, getSingleProviderService, updateProviderService } from "./controllers/providerService.controller.js"
-import { createNewService, deleteService, getAllServices, getSingleService, updateService } from "./controllers/service.controller.js"
-import { createAppointment, deleteAppointment, getAllAppointments, getSingleAppointment, updateAppointment } from "./controllers/appointment.controller.js"
-import { createNewReview, deleteReview, getAllReviews, getSingleUserReviews, updateReview } from "./controllers/review.controller.js"
 import userRouter from "./routes/user.routes.js"
 import serviceRouter from "./routes/service.routes.js"
 import reviewRouter from "./routes/review.routes.js"
@@ -25,6 +12,9 @@ import providerServiceRouter from "./routes/providerService.routes.js"
 import appointmentRouter from "./routes/appointment.routes.js"
 import auditRouter from "./routes/audit.routes.js"
 import cors from 'cors'
+import loginRouter from "./routes/auth.routes.js"
+import cookieParser from "cookie-parser"
+import cloudinary from "./config/cloudinary.config.js"
 
 dotenv.config()
 
@@ -33,12 +23,15 @@ const PORT = process.env.PORT || 5000
 app.use(cors())
 
 app.use(express.json()) //allows us to accept json data in the req.body
+app.use(cookieParser());
+
 app.use("/api/users", userRouter)
 app.use("/api/services", serviceRouter)
 app.use("/api/reviews", reviewRouter)
 app.use("/api/provider-services", providerServiceRouter)
 app.use("/api/audit-logs", auditRouter)
 app.use("/api/appointments", appointmentRouter)
+app.use("/api", loginRouter)
 
 //cron job
 // Start background job
@@ -64,10 +57,6 @@ export const appointmentIsActive = (appointmentObject) => {
     return false
 }
 
-//Login
-app.post("/api/login", async (req, res) => {
-    //login code here
-})
 
 app.listen(PORT, () => {
     connectDB()
