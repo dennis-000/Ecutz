a11da88442e33d32ecc75edeaac9753d45d33299
Dennis <odennisasiedu@gmail.com>
2024-11-30 14:15:28 -1200

    Made some changes at the backend for user and providers dash board and created the protected route to protect when both parties logsin or donot logsin

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,7 +1,12 @@
 
 const MyAccount = () => {
   return (
-    <div>MyAccount</div>
+    <div className="max w-[1170px] px-5 mx-auto">
+      <div className="grid md:grid-cols-3 gap-10">
+
+        <div className="pb-[50] px-[30]"></div>
+      </div>
+    </div>
   )
 }
 
